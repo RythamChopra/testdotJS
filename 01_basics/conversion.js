66d61d51406225f50d95dc5826b56edf17a12c40
RythamChopra <145897285+RythamChopra@users.noreply.github.com>
2025-09-24 08:18:56 +0000

    DT Conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,40 @@
+let score2 = true ;
+// console.log(typeof score2) ;
+
+let score2TypeNumber = Number(score2) ;
+// console.log(typeof score2TypeNumber) ; 
+
+// console.log(score2TypeNumber) ; 
+
+//33 => 33 ,
+//"33a" => NaN ,
+//true => 1 , false => 0
+
+let isLoggedIn = 1 ;
+
+let numberIsLoggedIn = Boolean(isLoggedIn) ;
+// console.log(typeof numberIsLoggedIn);
+// console.log(numberIsLoggedIn);
+
+
+
+let name1 = "" ;
+
+let nameToBoolean = Boolean(name1) ; 
+// console.log(typeof nameToBoolean);
+// console.log(nameToBoolean);
+
+
+
+let someNumber = 42 ;
+
+let someNumberToString = String(someNumber) ;
+// console.log(typeof someNumberToString);
+// console.log(someNumberToString);
+
+
+
+
+
+
+
