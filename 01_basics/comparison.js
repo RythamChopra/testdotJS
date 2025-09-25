17a8734eda130375e2b4ef5c9335a6ee6e2de7f1
RythamChopra <145897285+RythamChopra@users.noreply.github.com>
2025-09-25 07:57:40 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,47 @@
+
+
+// console.log("2" > 1 );
+// console.log(1 < "3");
+
+//== , >= , <> , == and comparison are treated differently in Javascript , the equality will not try to change the datatype while the
+//will try to change the data type . Example :
+
+/*
+console.log(null > 0); //false , Here null is converted into 0.
+console.log(null == 0); //false
+console.log(null >= 0); //true , Here null is converted into 0.
+*/
+
+//Incase of undefined , all comparisons will be false.
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+console.log("2" == 2 ); //true
+console.log("2" === 2 ); //strict check on values as well as datatypes , No conversion on === 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
